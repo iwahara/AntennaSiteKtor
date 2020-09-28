0b722bd3e2798f3e2d3135b4362a5deb8a6c0c70
Masaki Iwahara <3982522+iwahara@users.noreply.github.com>
2020-09-28 21:08:28 +0900

    articleのrepositoryを実装

---
+++ test/repository/ArticleRepositoryTest.kt
@@ -0,0 +1,61 @@
+package com.iwahara.antenna.ktor.repository
+
+import com.iwahara.antenna.ktor.entity.Article
+import com.iwahara.antenna.ktor.entity.Site
+import com.iwahara.antenna.ktor.test.DataBaseTest
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.joda.time.DateTime
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ArticleRepositoryTest : DataBaseTest() {
+
+    @BeforeTest
+    fun setUp() {
+        setUpMySQL("jdbc:mysql://localhost:3306", "root", "root", "test")
+    }
+
+    @AfterTest
+    fun tearDown() {
+        cleanUpMySQL("jdbc:mysql://localhost:3306", "root", "root", "test")
+    }
+
+    override fun fixture() {
+        Site.insert {
+            it[name] = "サイト名"
+            it[url] = "http://example.com/url"
+            it[feedUrl] = "http://example.com/feed"
+            it[articleCount] = 10
+            it[updatedAt] = DateTime(2020, 12, 12, 10, 10, 1)
+        }
+        for (i in 0 until 10) {
+            Article.insert {
+                it[name] = "記事${i}"
+                it[url] = "http://example.com/url${i}"
+                it[postDatetime] = DateTime(2020, 12, 12, 10, 10, i)
+                it[sortingOrder] = "$i".padStart(32, '0')
+                it[viewCount] = i
+                it[siteId] = 1
+            }
+        }
+    }
+
+    @Test
+    fun test_findBySite() {
+        val db = Database.connect("jdbc:mysql://localhost:3306/test", "com.mysql.jdbc.Driver", "root", "root")
+        transaction(db) {
+            val repository = ArticleRepository()
+            val actual = repository.findBySite(1, DateTime(2020, 12, 12, 10, 10, 8), 5)
+            assertEquals(5, actual.size)
+
+            for (i in 0 until 5) {
+                val article = actual[i]
+                assertEquals("${8 - i}".padStart(32, '0'), article.sortingOrder)
+            }
+        }
+    }
+}
