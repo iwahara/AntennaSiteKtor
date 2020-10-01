a0d3e413a231778cc8daa605a61c1c6091baf16c
Masaki Iwahara <3982522+iwahara@users.noreply.github.com>
2020-10-01 21:28:23 +0900

    テストの追加

---
+++ test/model/site/list/GetArticleListTest.kt
@@ -0,0 +1,31 @@
+package com.iwahara.antenna.ktor.model.site.list
+
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.joda.time.DateTime
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class GetArticleListTest {
+
+    @Test
+    fun test_get() {
+        val articleRepository = mockk<ArticleRepository>()
+        val postDatetime = DateTime.now()
+        val targetDatetime = DateTime.now()
+        val expected =
+            listOf(ArticleRepository.Data(1, "記事", "http://example.com", postDatetime, "202009120909091234", 1))
+
+        every { articleRepository.findBySite(1, targetDatetime, 10) } returns expected
+
+        val model = GetArticleList(articleRepository)
+        val actual = model.get(1, targetDatetime, 10)
+
+        assertEquals(1, actual.size)
+        assertEquals(expected, actual)
+        verify(exactly = 1) { articleRepository.findBySite(1, targetDatetime, 10) }
+        confirmVerified(articleRepository)
+    }
+}
