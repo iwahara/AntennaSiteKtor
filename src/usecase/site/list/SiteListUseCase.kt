110d18a7e3ba07285da7c2650087623c0cab2a5f
Masaki Iwahara <3982522+iwahara@users.noreply.github.com>
2020-10-03 00:10:57 +0900

    usecaseの実装

--- src/usecase/site/list/SiteListUseCase.kt
+++ src/usecase/site/list/SiteListUseCase.kt
@@ -1,4 +1,19 @@
 package com.iwahara.antenna.ktor.usecase.site.list
 
-class SiteListUseCase() {
+import com.iwahara.antenna.ktor.model.site.list.ArticleRepository
+import com.iwahara.antenna.ktor.model.site.list.SiteRepository
+import org.joda.time.DateTime
+
+class SiteListUseCase(private val siteList: SiteList, private val articleList: ArticleList) {
+    data class Data(val siteData: SiteRepository.Data, val articleList: List<ArticleRepository.Data>)
+
+    fun get(targetDatetime: DateTime, count: Int): List<Data> {
+        val ret = mutableListOf<Data>()
+        val siteList = siteList.get()
+        for (site in siteList) {
+            val articleList = articleList.get(site.id, targetDatetime, count)
+            ret.add(Data(site, articleList))
+        }
+        return ret
+    }
 }
