21e7acdda62bca86ba64e2a0bef134c1edcd71dc
Masaki Iwahara <3982522+iwahara@users.noreply.github.com>
2020-10-09 23:47:03 +0900

    データベース接続周りを整理

--- src/database/DataBaseConnectionInfo.kt
+++ src/database/DataBaseConnectionInfo.kt
@@ -3,8 +3,6 @@ package com.iwahara.antenna.ktor.database
 data class DataBaseConnectionInfo(
     val driver: String,
     val url: String,
-    val port: String,
-    val name: String,
     val user: String,
     val password: String
 )
--- src/database/DataBaseSettings.kt
+++ src/database/DataBaseSettings.kt
@@ -5,8 +5,8 @@ import org.jetbrains.exposed.sql.Database
 class DataBaseSettings(info: DataBaseConnectionInfo) {
     val db by lazy {
         Database.connect(
-            "${info.url}:${info.port}/${info.name}",
-            driver = info.driver,//"com.mysql.jdbc.Driver",
+            driver = info.driver,
+            url = info.url,
             user = info.user,
             password = info.password
         )
