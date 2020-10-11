70ba2b7929c88d61c9cb7fd3c43253f386222bb0
Masaki Iwahara <3982522+iwahara@users.noreply.github.com>
2020-10-11 21:53:41 +0900

    viewの分離

--- src/controller/SiteListController.kt
+++ src/controller/SiteListController.kt
@@ -1,14 +1,10 @@
 package com.iwahara.antenna.ktor.controller
 
 import com.iwahara.antenna.ktor.usecase.site.list.SiteListUseCase
-import com.iwahara.antenna.ktor.view.MainTemplate
+import com.iwahara.antenna.ktor.view.site.ListView
 import io.ktor.application.*
 import io.ktor.html.*
 import io.ktor.routing.*
-import kotlinx.html.a
-import kotlinx.html.classes
-import kotlinx.html.div
-import kotlinx.html.p
 import org.koin.ktor.ext.inject
 
 
@@ -16,52 +12,6 @@ fun Routing.siteList() {
     val useCase by inject<SiteListUseCase>()
     val siteArticleDataList = useCase.get(10)
     get("/site.html") {
-        call.respondHtmlTemplate(MainTemplate()) {
-            title {
-                text("サイト別一覧")
-            }
-            content {
-                div {
-                    classes = setOf("row row-eq-height")
-                    for (siteArticleData in siteArticleDataList) {
-                        div {
-                            classes = setOf("col-xs-12 col-md-6")
-                            div {
-                                classes = setOf("panel panel-info")
-                                div {
-                                    classes = setOf("panel-heading")
-                                    a {
-                                        href = "/site/${siteArticleData.siteData.id}"
-                                        text(siteArticleData.siteData.name)
-                                    }
-                                }
-                                div {
-                                    classes = setOf("list-group")
-                                    for (article in siteArticleData.articleList) {
-                                        a {
-                                            classes = setOf("list-group-item")
-                                            href = article.url
-                                            target = "_blank"
-                                            p {
-                                                classes = setOf("list-group-item-heading")
-                                                text(article.name)
-                                            }
-                                            p {
-                                                classes = setOf("list-group-item-text")
-                                                text(siteArticleData.siteData.name)
-                                            }
-                                            p {
-                                                classes = setOf("list-group-item-text")
-                                                text(article.postDatetime.toString("yyyy-MM-dd HH:mm:ss"))
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        call.respondHtmlTemplate(ListView(siteArticleDataList)) {}
     }
 }
