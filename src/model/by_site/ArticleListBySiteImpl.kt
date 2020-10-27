a870fb3bfa672289afe085d25313726328c7e792
Masaki Iwahara <3982522+iwahara@users.noreply.github.com>
2020-10-27 21:40:51 +0900

    repositoryのdataをmodelで変換するように

--- src/model/by_site/ArticleListBySiteImpl.kt
+++ src/model/by_site/ArticleListBySiteImpl.kt
@@ -4,11 +4,15 @@ import com.iwahara.antenna.ktor.usecase.by_site.ArticleListBySite
 import org.joda.time.DateTime
 
 class ArticleListBySiteImpl(private val articleRepository: ArticleBySiteRepository) : ArticleListBySite {
-    override fun get(siteId: Int, targetDatetime: DateTime, count: Int): List<ArticleBySiteRepository.Data> {
-        return this.articleRepository.findBySite(siteId, targetDatetime, count)
+    override fun get(siteId: Int, targetDatetime: DateTime, count: Int): List<ArticleListBySite.Data> {
+        return this.articleRepository.findBySite(siteId, targetDatetime, count).map { convert(it) }
     }
 
-    override fun get(siteId: Int, targetDatetime: DateTime, count: Int, offset: String): List<ArticleBySiteRepository.Data> {
-        return this.articleRepository.findBySite(siteId, targetDatetime, count, offset)
+    override fun get(siteId: Int, targetDatetime: DateTime, count: Int, offset: String): List<ArticleListBySite.Data> {
+        return this.articleRepository.findBySite(siteId, targetDatetime, count, offset).map { convert(it) }
+    }
+
+    private fun convert(origin: ArticleBySiteRepository.Data): ArticleListBySite.Data {
+        return ArticleListBySite.Data(origin.id, origin.name, origin.url, origin.postDatetime, origin.sortingOrder, origin.viewCount)
     }
 }
